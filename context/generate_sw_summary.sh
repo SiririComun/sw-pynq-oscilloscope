99916b1a2c05dd7934b966509c2fe83e938efd1b
SiririComun <pablo.sanchez2@udea.edu.co>
2026-08-19 11:05:03 -0500

    test(notebook): add 05_audio_dashboard notebook for dedicated audio instrument

--- context/generate_sw_summary.sh
+++ context/generate_sw_summary.sh
@@ -69,7 +69,7 @@ TARGET_FILES=(
     "notebooks/02_xadc_getting_started.ipynb:json"
     "notebooks/03_oscilloscope_dashboard.ipynb:json"
     "notebooks/04_fft_spectrum_analyzer.ipynb:json"
-    "notebooks/05_dual_channel_test.ipynb:json"
+    "notebooks/05_audio_dashboard.ipynb:json"
     "notebooks/06_audio_fft_microphone_test.ipynb:json"
     "context/generate_sw_summary.sh:bash"
 )
